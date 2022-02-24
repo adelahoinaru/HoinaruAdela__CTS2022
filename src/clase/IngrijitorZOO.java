e8fd8e6dd8b16aad77c4bd613fd03379f0b184e0
adelahoinaru <hoinaruadela19@stud.ase.ro>
2022-02-24 13:45:28 +0200

    Initial commit

---
+++ src/clase/IngrijitorZOO.java
@@ -0,0 +1,23 @@
+package clase;
+
+public class IngrijitorZOO {
+
+    private String nume;
+
+    public IngrijitorZOO(String nume) {
+        this.nume = nume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public void hranesteAnimal(Animal animal, String mancare) {
+        animal.mananca(mancare);
+    }
+
+}
